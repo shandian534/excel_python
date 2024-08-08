45f4734c5357e4598371fc0ce17659c395f0fbbb
刘平贵 <985346375@sina.com>
2024-08-08 17:54:53 +0800

    对excel模向去重

--- remove_duplication.py
+++ remove_duplication.py
@@ -1,16 +1,22 @@
-import os
-import openpyxl
+
+import pandas as pd
+
 
 if __name__ == '__main__':
-    path = r"C:\Users\Acer\Desktop"
-    os.chdir(path)  # 修改工作路径
-
-    workbook = openpyxl.load_workbook('重复1.xlsx')  # 返回一个workbook数据类型的值
-    print(workbook.sheetnames)  # 打印Excel表中的所有表
-    sheet = workbook.active  # 获取活动表
-    print(sheet)
-    # 结果：
-    # ['Sheet1', 'Sheet2']
-
-
-
+    sheet_name = 'Sheet1'  # 替换为你的工作表名称
+    file_path_out = r'C:\Users\Acer\Desktop\重复2.xlsx'
+    result = pd.DataFrame()
+    with pd.ExcelWriter(file_path_out, engine='xlsxwriter') as writer:
+        # 数据传给Excel的writer
+        result.to_excel(writer, index=False, sheet_name='Sheet1')
+        # 再从writer加载回该sheet
+        worksheet = writer.sheets[sheet_name]
+        # 指定列宽设置的范围
+        worksheet.set_column("D:N", 20)
+        # 循环每一列的列序号，设置列宽为20个单位（指Excel中的列宽单位）  这种方法也行
+        for idx in range(result.shape[1]):
+            worksheet.set_column(idx, idx, 20)
+        # 设置缩放比例
+        worksheet.set_zoom(zoom=125)
+        # writer保存数据到本地
+        writer._save()
