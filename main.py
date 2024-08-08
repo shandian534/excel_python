6b5e4280d5530c5a31c1acf753e1c864506a7a78
刘平贵 <985346375@sina.com>
2024-08-08 17:38:13 +0800

    对excel模向去重

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from xlsxwriter import Workbook
 
 # 读取Excel文件
 
@@ -27,3 +28,19 @@ if __name__ == '__main__':
                 col_val[cell_value] = cell_value
             print(f'Row {row + 1}, Column {col + 1}: {cell_value}')
     df.to_excel(file_path_out, sheet_name='Sheet1', index=False)
+
+    result = pd.DataFrame()
+    with pd.ExcelWriter(file_path_out, engine='xlsxwriter') as writer:
+        # 数据传给Excel的writer
+        result.to_excel(writer, index=False, sheet_name='Sheet1')
+        # 再从writer加载回该sheet
+        worksheet = writer.sheets[sheet_name]
+        # 指定列宽设置的范围
+        worksheet.set_column("D:N", 20)
+        # 循环每一列的列序号，设置列宽为20个单位（指Excel中的列宽单位）  这种方法也行
+        for idx in range(result.shape[1]):
+            worksheet.set_column(idx, idx, 20)
+        # 设置缩放比例
+        worksheet.set_zoom(zoom=125)
+        # writer保存数据到本地
+        writer._save()
